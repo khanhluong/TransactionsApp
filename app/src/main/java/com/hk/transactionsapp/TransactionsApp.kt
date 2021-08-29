4db1598a7c39cc2236e6cab6c8c61d127ddbe8e8
khanh.luong <baokhanh2710cntt@gmail.com>
2021-08-29 18:11:32 +1200

    add gradle, create directory package

---
+++ app/src/main/java/com/hk/transactionsapp/TransactionsApp.kt
@@ -0,0 +1,6 @@
+package com.hk.transactionsapp
+
+import android.app.Application
+
+class TransactionsApp : Application() {
+}
---
+++ app/src/main/java/com/hk/transactionsapp/features/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hk.transactionsapp.features
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hk.transactionsapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
